169463f0d9997539c0b25f90e3f4c38e6a7435e9
Adam Taitano <adam.taitano@gmail.com>
2018-06-23 01:30:46 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,8 +1,15 @@
 def using_until
-    levitation_force = 6
-    until levitation_force >= 10
-      levitation_force += 1
-      puts "Wingardium Leviosa"
-    end
+  levitation_force = 6
+  until levitation_force >= 10
+    levitation_force += 1
+    puts "Wingardium Leviosa"
+  end
 end
 
+def using_while
+  levitation_force = 6
+  while levitation_force < 10
+    levitation_force += 1
+    puts "Wingardium Leviosa"
+  end
+end
